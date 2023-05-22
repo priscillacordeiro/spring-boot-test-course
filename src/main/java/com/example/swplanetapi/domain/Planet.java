39f4bcd62272eeef760d59f87097d0da04ac9604
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2023-05-22 17:27:58 +0100

    add JPA annotation

--- src/main/java/com/example/swplanetapi/domain/Planet.java
+++ src/main/java/com/example/swplanetapi/domain/Planet.java
@@ -1,8 +1,19 @@
 package com.example.swplanetapi.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "planets")
 public class Planet {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String climate;
     private String terrain;
@@ -10,24 +21,31 @@ public class Planet {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getClimate() {
         return climate;
     }
+
     public void setClimate(String climate) {
         this.climate = climate;
     }
+
     public String getTerrain() {
         return terrain;
     }
+
     public void setTerrain(String terrain) {
         this.terrain = terrain;
     }
